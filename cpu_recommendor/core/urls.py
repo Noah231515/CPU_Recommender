9d03f0bba59315e4976c9d5a3e4f0a80304291cf
Noah <noah231515@gmail.com>
2020-08-22 11:48:28 -0400

    create angular project

--- cpu_recommendor/core/urls.py
+++ cpu_recommendor/core/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.conf.urls import url
+from django.contrib import admin
+from django.views.generic.base import TemplateView
+from core.views import *
 
 from . import views
 
 urlpatterns = [
     path('',views.IndexView.as_view(), name='index'),
-    path('recommendation/', views.Recommendation_Page)
-
+    path('recommendation/', views.Recommendation_Page),
+    path(r'^.*', TemplateView.as_view(template_name="home.html"), name="home")
 ]
